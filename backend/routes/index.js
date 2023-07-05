15f595ed9d2400d07587f93172ee5de9845be5e5
Darian <brooksd@alumni.stanford.edu>
2023-07-05 13:13:21 -0700

    Add Api Router (Auth Me Phase 1)

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 // test route
 const express = require("express");
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
