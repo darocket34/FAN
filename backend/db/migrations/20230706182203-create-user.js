852a18154e7dac2b24440fe72c874a9d9959e28a
Darian <brooksd@alumni.stanford.edu>
2023-07-06 11:41:46 -0700

    Add Users table migration and seeds

---
+++ backend/db/migrations/20230706182203-create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production'){
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+        unique: true,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(256),
+        unique: true
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY,
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
+  }
+};
