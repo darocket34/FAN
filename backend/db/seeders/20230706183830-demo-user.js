e9363333aa5e49b1161a733715bf6f35ceed7f5b
Darian <brooksd@alumni.stanford.edu>
2023-07-07 12:36:44 -0700

    Backend Ready for Deployment

--- backend/db/seeders/20230706183830-demo-user.js
+++ backend/db/seeders/20230706183830-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
     await User.bulkCreate(
       [
         {
+          firstName: "Rodney",
+          lastName: "Brown",
           email: "demo@user.io",
           username: "Demo-lition",
           hashedPassword: bcrypt.hashSync("password"),
         },
         {
+          firstName: "Leyroy",
+          lastName: "Jenkins",
           email: "user1@user.io",
           username: "FakeUser1",
           hashedPassword: bcrypt.hashSync("password2"),
         },
         {
+          firstName: "Signey",
+          lastName: "Yellow",
           email: "user2@user.io",
           username: "FakeUser2",
           hashedPassword: bcrypt.hashSync("password3"),
